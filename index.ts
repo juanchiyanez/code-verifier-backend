6611ce89acd6a2c4719cd65150877010d213f0da
learnissogood <juanchi097@gmail.com>
2023-07-11 12:40:32 -0300

    Create route goodbye and modify the route hello to receive a query arameter and returns a 200 response with a specific json message

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
+import { request } from "http";
 
 // Configuration the .env file
 dotenv.config();
 
 // Create Express APP
 const app: Express = express();
-const port: string | number = process.env.PORT || 8000;
+const port: string | number = process.env.PORT ?? 8000;
 
 // Define the first route of App
 app.get('/', (req: Request, res: Response) => {
@@ -16,7 +17,21 @@ app.get('/', (req: Request, res: Response) => {
 
 // Define a second route of App
 app.get('/hello', (req: Request, res: Response) => {
-    res.send("Hello from /hello");
-})
+    const name = req.query.name ?? 'anónimo';
+    res.status(200).json({
+        data: {
+            message: `Hola, ${name}`
+        }
+    });
+});
+
+app.get('/goodbye', (req: Request, res: Response) => {
+    res.status(200).json({
+        data: {
+            message: 'Goodbye, world'
+        }
+    })
+});
+
 // Execute App and listen request to PORT
 app.listen(port, () => console.log(`EXPRESS SERVER: Running at http://localhost:${port}`));
