b9095528062dd9d7c47abf71a36a2eda14cff8db
learnissogood <juanchi097@gmail.com>
2023-07-11 18:57:05 -0300

    Server Config, Cors and Helmet config

--- index.ts
+++ index.ts
@@ -1,37 +1,18 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
-import { request } from "http";
+import server from './src/server';
+import { LogSuccess, LogError } from './src/utils/logger';
 
-// Configuration the .env file
+// * Config the .env file
 dotenv.config();
 
-// Create Express APP
-const app: Express = express();
-const port: string | number = process.env.PORT ?? 8000;
+const port = process.env.PORT ?? 8000;
 
-// Define the first route of App
-app.get('/', (req: Request, res: Response) => {
-    // Send Hello World
-    res.send('Welcome to API Restful: Express + Ts + Nodemon + Jest + Swagger + Mongoose');
+// * Execute Server
+server.listen(port, () => {
+    LogSuccess(`[SERVER ON]: Running in http://localhost:${port}/api`);
 });
 
-// Define a second route of App
-app.get('/hello', (req: Request, res: Response) => {
-    const name = req.query.name ?? 'anónimo';
-    res.status(200).json({
-        data: {
-            message: `Hola, ${name}`
-        }
-    });
-});
-
-app.get('/goodbye', (req: Request, res: Response) => {
-    res.status(200).json({
-        data: {
-            message: 'Goodbye, world'
-        }
-    })
-});
-
-// Execute App and listen request to PORT
-app.listen(port, () => console.log(`EXPRESS SERVER: Running at http://localhost:${port}`));
+// * Control SERVER ERROR
+server.on('error', (error) => {
+    LogError(`[SERVER ERROR]: ${error}`);
+})
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,38 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+
+// * Security
+import cors from 'cors';
+import helmet from 'helmet';
+
+// TODO: HTTPS
+
+// * Root Router
+import rootRouter from '../routes';
+
+// * Create Express App 
+const server: Express = express();
+
+// * Define SERVER to use "/api" and use rootRouter from  "index.ts" in routes
+// From this point onover: http://localhost:8000/api/
+server.use(
+    '/api',
+    rootRouter
+    );
+
+// TODO: Mongoose Connection
+
+// * Server Config
+server.use(helmet());
+server.use(cors());
+
+// Content Type Config
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
+
+// * Redirections
+// http://localhost:8000/ --> http://localhost:8000/api/
+server.get('/', (req: Request, res: Response) => {
+    res.redirect('/api');
+});
+
+export default server;
